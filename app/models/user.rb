ab0245c47a9e627a942bcf39f850b10584c8a52b
shownola <swasieko@gmail.com>
2020-12-11 20:35:10 -0700

    Complete comments using javascript and change devise to user permitted parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,30 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_one_attached :avatar
+  validates :username, presence: :true, uniqueness: {case_sensitive: false}
 
-  def username
-    return email.split('@')[0].capitalize
+  attr_writer :login
+
+  def validate_username
+    errors.add(:username, :invalid) if User.where(email: username).exists?
   end
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = value', {value: login.downcase}]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  # def username
+  #   return email.split('@')[0].capitalize
+  # end
 
   def comment_created
     self.number_of_comments = number_of_comments + 1
